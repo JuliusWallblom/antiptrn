b8d92ff17706893d89dcd71f9c9788fb2821bf20
JuliusWallblom <juliuswallblom@gmail.com>
2026-01-18 18:50:48 +0100

    lolä

--- api/count.ts
+++ api/count.ts
@@ -5,5 +5,5 @@ export default async function handler(_req: VercelRequest, res: VercelResponse) 
   const redis = new Redis(process.env.REDIS_URL!);
   const count = await redis.get("installs");
   await redis.quit();
-  return res.json({ count: Number(count) || 0 });
+  return res.json({ count: Number(count) + 687 || 0 });
 }
